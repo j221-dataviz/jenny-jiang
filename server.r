03de8b50303fdb1424b00f8a0144255756f14ff3
jennyjiang95 <jennyjiang95@berkeley.edu>
2017-12-03 22:24:54 -0800

    minor change server

--- server.r
+++ server.r
@@ -47,37 +47,36 @@ shinyServer(function(input, output) {
            y = "Total Number of Workers") +
       geom_text(aes_string(x="year",
                     y="sum_number",
-                    label = paste0("sum_number")), 
+                    label = paste0("sum_number")),
                 size=5,
                 colour= "#000000") +
       theme_minimal(base_size = 12, base_family = "Georgia") +
-      theme(plot.title = element_text(size = 20, family = "Georgia", face = "bold"), 
+      theme(plot.title = element_text(size = 20, family = "Georgia", face = "bold"),
             text=element_text(family="Times New Roman", size = 20),
-            axis.text.x=element_text(colour="black", size = 15), 
+            axis.text.x=element_text(colour="black", size = 15),
             axis.text.y=element_text(colour="black", size = 15)) +
       theme(legend.position="bottom")
 
-    # cleaned1 <- total_summary_degrees[employment == input$question, ] 
-    
+
     
     ## the plot won't show in the shiny app dashboard. it shows in the viewer
     
     
-    # cols <- brewer.pal(7, "Set1")
+     # cols <- brewer.pal(7, "Set1")
 
-    
-    # highchart() %>% 
-    #    hc_title(text = input$question) %>% 
-    #    hc_add_series_scatter(x = cleaned1$year, y = cleaned1$sum_number, 
+
+    # highchart() %>%
+    #    hc_title(text = input$question) %>%
+    #    hc_add_series_scatter(x = cleaned1$year, y = cleaned1$sum_number,
     #                          color = cleaned1$edu, name = input$question,
-    #                          group = cleaned1$edu) %>% 
-    #   hc_chart(style = list(fontFamily = "Georgia", 
+    #                          group = cleaned1$edu) %>%
+    #   hc_chart(style = list(fontFamily = "Georgia",
     #                         fontWeight = "bold")) %>%
     #   hc_xAxis(title = list(text="Year")) %>%
     #   hc_yAxis(title = list(text="Total Number of Workers")) %>%
     #   hc_plotOptions(series = list(marker = list(enabled = TRUE, symbol = "circle")))
-    #   
-    
+    #
+
     
     ############### none of the below works. 
     #hchart(cleaned1, "scatter", x = year, y = sum_number)
