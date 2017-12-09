699d9e673d398353607b285f18959050aad13f50
jennyjiang95 <jennyjiang95@berkeley.edu>
2017-12-08 16:27:30 -0800

    updates

--- server.r
+++ server.r
@@ -40,6 +40,7 @@ shinyServer(function(input, output) {
       filter(employment == input$question)
     
     ggplot(cleaned1) +
+      scale_fill_brewer(palette = "Set1", name ="") +
       geom_bar(aes_string(x = "year", y = "sum_number",  fill = "Edu") 
                ,stat = "identity") +
       labs(x = "Year",
@@ -52,74 +53,64 @@ shinyServer(function(input, output) {
       theme(legend.position="bottom")
     
   })
-    
   
-  output$Plot2 <- renderPlot({
+  
+  output$hcontainer <- renderHighchart({
+    
     
     cleaned1 <- total_summary2 %>%
       filter(employment == input$question)
 
-    
-    ### GGPLOT WORKS!
-    ggplot(cleaned1, aes_string(x = "year", y = "sum_number", color = "Education_Level", group = "Education_Level")) +
-      geom_point() +
-      geom_line() +
-      labs(title = input$question,
-           x = "Year",
-           y = "Total Number of Workers (thousands)") +
-      geom_text(aes_string(x="year",
-                    y="sum_number",
-                    label = paste0("sum_number")),
-                size=5,
-                colour= "#000000") +
-      theme_minimal(base_size = 12, base_family = "Georgia") +
-      theme(plot.title = element_text(size = 20, family = "Georgia", face = "bold"),
-            text=element_text(family="Times New Roman", size = 20),
-            axis.text.x=element_text(colour="black", size = 15),
-            axis.text.y=element_text(colour="black", size = 15)) +
-      theme(legend.position="bottom")
-
-
-    
-    ## the plot won't show in the shiny app dashboard. it shows in the viewer
+    # hchart(cleaned1, "scatter", x = year, y = sum_number)
     
     
-     # cols <- brewer.pal(7, "Set1")
-
-
-    # highchart() %>%
-    #    hc_title(text = input$question) %>%
-    #    hc_add_series_scatter(x = cleaned1$year, y = cleaned1$sum_number,
-    #                          color = cleaned1$edu, name = input$question,
-    #                          group = cleaned1$edu) %>%
-    #   hc_chart(style = list(fontFamily = "Georgia",
-    #                         fontWeight = "bold")) %>%
-    #   hc_xAxis(title = list(text="Year")) %>%
-    #   hc_yAxis(title = list(text="Total Number of Workers")) %>%
-    #   hc_plotOptions(series = list(marker = list(enabled = TRUE, symbol = "circle")))
-    #
+    cols <- brewer.pal(3, "Set1")
+    
+    hc<- hchart(cleaned1, "line", hcaes(x = year, y = sum_number, group = Edu)) %>%
+      hc_colors(cols) %>% 
+      hc_chart(style = list(fontFamily = "Georgia",
+                          fontWeight = "bold")) %>%
+      hc_xAxis(title = list(text="Year")) %>%
+      hc_yAxis(title = list(text="Total Number of Workers (thousands)")) %>% 
+      hc_title(text = input$question) %>%
+      hc_plotOptions(series = list(marker = list(enabled = TRUE, symbol = "circle")))
+    
+    hc
+    
 
     
-    ############### none of the below works. 
-    #hchart(cleaned1, "scatter", x = year, y = sum_number)
-    # 
-    # highchart() %>% 
-    #   hc_title(text = input$question) %>% 
-    #   hc_add_series_scatter(x = cleaned1$year, y = cleaned1$sum_number, color = cleaned1$edu, name = input$question)
-      # hc_add_series(cleaned1, type = "line", hcaes(x = year, y = sum_number, color = edu))
-
-    
-    
-    # hchart(cleaned1, "line", hcaes(x = "year", y = "sum_number", color = "edu", group = "edu")) %>%
-    #   hc_colors(cols) 
-    #   hc_chart(style = list(fontFamily = "Georgia",
-    #                         fontWeight = "bold")) %>%
-    #   hc_xAxis(title = list(text="Year")) %>%
-    #   hc_yAxis(title = list(text="Total Number of Workers")) %>%
-    #   hc_plotOptions(series = list(marker = list(enabled = TRUE, symbol = "circle")))
-
-
-
   })
-  
+    
+  # 
+  # output$Plot2 <- renderPlot({
+  #   
+  #   cleaned1 <- total_summary2 %>%
+  #     filter(employment == input$question)
+  # 
+  #   
+  #   ### GGPLOT WORKS!
+  #   ggplot(cleaned1, aes_string(x = "year", y = "sum_number", color = "Education_Level", group = "Education_Level")) +
+  #     scale_color_brewer(palette = "Set1", name ="") +
+  #     geom_point() +
+  #     geom_line() +
+  #     labs(title = input$question,
+  #          x = "Year",
+  #          y = "Total Number of Workers (thousands)") +
+  #     geom_text(aes_string(x="year",
+  #                   y="sum_number",
+  #                   label = paste0("sum_number")),
+  #               size=5,
+  #               colour= "#000000") +
+  #     theme_minimal(base_size = 12, base_family = "Georgia") +
+  #     theme(plot.title = element_text(size = 20, family = "Georgia", face = "bold"),
+  #           text=element_text(family="Times New Roman", size = 20),
+  #           axis.text.x=element_text(colour="black", size = 15),
+  #           axis.text.y=element_text(colour="black", size = 15)) +
+  #     theme(legend.position="bottom")
+  # 
+  # 
+  # 
+  # 
+  # })
+  # 
 })
